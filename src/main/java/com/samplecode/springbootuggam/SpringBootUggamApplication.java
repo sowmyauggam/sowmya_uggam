12e02637e865b0efbac8cb9039a162850335b970
sowmyauggam <164959632+sowmyauggam@users.noreply.github.com>
2024-03-25 19:13:32 -0500

    first commit

---
+++ src/main/java/com/samplecode/springbootuggam/SpringBootUggamApplication.java
@@ -0,0 +1,13 @@
+package com.samplecode.springbootuggam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUggamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUggamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samplecode/springbootuggam/SpringBootUggamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samplecode.springbootuggam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUggamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
